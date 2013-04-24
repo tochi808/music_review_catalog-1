68cc956e12affd700d22c6f64426b938a617cafc
kjugk <k02ugk@gmail.com>
2013-04-24 13:36:01 +0900

    ログイン関連メッセージを表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def login_notice_message
+    if flash[:notice] then
+      content_tag :div, flash[:notice], :class => "label label-info"
+    end
+  end 
+
+  def login_alert_message
+    if flash[:alert] then
+      content_tag :div, flash[:alert], :class => "label label-warning"
+    end
+  end
 end
